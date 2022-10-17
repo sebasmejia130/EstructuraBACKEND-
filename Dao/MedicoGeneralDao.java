5bb923d33c7cb3bcb8eb45b0fe3fd6936b83ed49
jsebastianml555 <81388677+jsebastianml555@users.noreply.github.com>
2022-10-17 08:58:56 -0500

    Add files via upload

---
+++ Dao/MedicoGeneralDao.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.appmedica.appmedica.Dao;
+
+import com.appmedica.appmedica.Modelo.MedicoGeneral;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoGeneralDao extends JpaRepository<MedicoGeneral, Long>{
+    
+}
---
+++ Dao/OrdenesRemisionesDao.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.appmedica.appmedica.Dao;
+
+import com.appmedica.appmedica.Modelo.OrdenesRemisiones;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdenesRemisionesDao extends JpaRepository<OrdenesRemisiones, Long> {
+    
+}
